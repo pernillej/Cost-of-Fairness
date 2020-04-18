c5d8f302e24eb66623e9912aebe43c3e49bc721f
Pernille Johnsen <pernillej11@gmail.com>
2020-04-18 15:36:06 +0200

    Moved run configurations from experiment files to main file.

--- src/experiments/reweighing.py
+++ src/experiments/reweighing.py
@@ -1,67 +1,68 @@
 from src.nsga2.nsga2 import nsga2
 from src.nsga2.population import get_C, get_gamma, get_selected_features
-from src.metrics import statistical_parity_difference, function_name_to_string
-from src.data import load_german_dataset
+from src.metrics import function_name_to_string
 from src.algorithms import svm_reweighing
 from src.util.filehandler import write_result_to_file
 
-NUM_GENERATIONS = 10
-POPULATION_SIZE = 10
-MUTATION_RATE = 0.05
-CROSSOVER_RATE = 0.7
-CHROMOSOME_LENGTH = 30 + 57  # 15 each for C and gamma, plus 57 for the number of features in german data set
-FAIRNESS_METRIC = statistical_parity_difference
-DATA_SET = load_german_dataset()
-PRIVILEGED_GROUPS = [{'age': 1}]
-UNPRIVILEGED_GROUPS = [{'age': 0}]
 
-
-def svm_reweighing_experiment():
+def svm_reweighing_experiment(num_generations, population_size, mutation_rate, crossover_rate, chromosome_length,
+                              fairness_metric, data_set, privileged_groups, unprivileged_groups):
     """
     SVM with Reweighing experiment.
 
+    :param num_generations: Number of generations for NSGA-II
+    :param population_size: Population size for NSGA-II
+    :param mutation_rate: Mutation rate for NSGA-II
+    :param crossover_rate: Crossover probability for NSGA-II
+    :param chromosome_length: Length of chromosome used in NSGA-II
+    :param fairness_metric: Fairness metric used to calculate the fairness score
+    :param data_set: The data set to run experiment on
+    :param privileged_groups: The privileged groups in the data set
+    :param unprivileged_groups: The unprivileged groups in the data set
     :return: Resulting Pareto front
     """
-    result = nsga2(pop_size=POPULATION_SIZE,
-                   num_generations=NUM_GENERATIONS,
-                   chromosome_length=CHROMOSOME_LENGTH,
-                   crossover_rate=CROSSOVER_RATE,
-                   mutation_rate=MUTATION_RATE,
+
+    # Defines the evaluation function
+    def evaluation_function(chromosome):
+        """
+        Function the be used to evaluate the NSGA-II chromosomes and return fitness scores.
+        Contains the svm with reweighing algorithm, that returns the fitness scores for the specified metrics.
+
+        :param chromosome: Chromosome to specify parameters for SVM
+        :return: The fitness scores in a list: [accuracy_score, fairness_score]
+        """
+        # Check if scores already calculated for identical chromosome, in which case return those scores
+        if str(chromosome) in FITNESS_SCORES:
+            return FITNESS_SCORES[str(chromosome)]
+        else:
+            C = get_C(chromosome)
+            gamma = get_gamma(chromosome)
+            selected_features = get_selected_features(chromosome, 30)
+            accuracy_score, fairness_score = svm_reweighing(dataset=data_set, fairness_metric=fairness_metric,
+                                                            C=C, gamma=gamma, keep_features=selected_features,
+                                                            privileged_groups=privileged_groups,
+                                                            unprivileged_groups=unprivileged_groups)
+            FITNESS_SCORES[str(chromosome)] = [accuracy_score, fairness_score]
+            return [accuracy_score, fairness_score]
+
+    # Runs NSGA-II
+    result = nsga2(pop_size=population_size,
+                   num_generations=num_generations,
+                   chromosome_length=chromosome_length,
+                   crossover_rate=crossover_rate,
+                   mutation_rate=mutation_rate,
                    evaluation_algorithm=evaluation_function)
 
-    # Summary to write to file
+    # Writes summary to file
     result_summary = {'name': 'SVM_Reweighing',
                       'result': result,
-                      'fairness_metric': function_name_to_string(FAIRNESS_METRIC),
-                      'nsga2_parameters': {'num_generations': NUM_GENERATIONS, 'population_size': POPULATION_SIZE,
-                                           'crossover_rate': CROSSOVER_RATE, 'mutation_rate': MUTATION_RATE,
-                                           'chromosome_length': CHROMOSOME_LENGTH}}
+                      'fairness_metric': function_name_to_string(fairness_metric),
+                      'nsga2_parameters': {'num_generations': num_generations, 'population_size': population_size,
+                                           'crossover_rate': crossover_rate, 'mutation_rate': mutation_rate,
+                                           'chromosome_length': chromosome_length}}
     write_result_to_file(result_summary, "svm_reweighing")
     # Return only the result, not the summary
     return result
-
-
-def evaluation_function(chromosome):
-    """
-    Function the be used to evaluate the NSGA-II chromosomes and return fitness scores.
-    Contains the svm with reweighing algorithm, that returns the fitness scores for the specified metrics.
-
-    :param chromosome: Chromosome to specify parameters for SVM
-    :return: The fitness scores in a list: [accuracy_score, fairness_score]
-    """
-    # Check if scores already calculated for identical chromosome, in which case return those scores
-    if str(chromosome) in FITNESS_SCORES:
-        return FITNESS_SCORES[str(chromosome)]
-    else:
-        C = get_C(chromosome)
-        gamma = get_gamma(chromosome)
-        selected_features = get_selected_features(chromosome, 30)
-        accuracy_score, fairness_score = svm_reweighing(dataset=DATA_SET, fairness_metric=FAIRNESS_METRIC,
-                                                        C=C, gamma=gamma, keep_features=selected_features,
-                                                        privileged_groups=PRIVILEGED_GROUPS,
-                                                        unprivileged_groups=UNPRIVILEGED_GROUPS)
-        FITNESS_SCORES[str(chromosome)] = [accuracy_score, fairness_score]
-        return [accuracy_score, fairness_score]
 
 
 """ 
--- src/main.py
+++ src/main.py
@@ -5,6 +5,9 @@ from src.experiments.calibrated_eq_odds import svm_caleqodds_experiment
 from src.experiments.disparate_impact_remover import svm_dir_experiment
 from src.util.plotter import plot_results
 from src.util.filehandler import read_result_from_file
+from src.data import load_german_dataset
+from src.metrics import statistical_parity_difference, theil_index, disparate_impact, average_odds_difference, \
+    equal_opportunity_difference
 
 """
 Done: 
@@ -19,22 +22,56 @@ TODO:
 Customize...
 """
 
+"""
+Configuration
+"""
+
+NUM_GENERATIONS = 20
+POPULATION_SIZE = 20
+MUTATION_RATE = 0.05
+CROSSOVER_RATE = 0.7
+CHROMOSOME_LENGTH = 30 + 57  # 15 each for C and gamma, plus 57 for the number of features in german data set
+FAIRNESS_METRIC = statistical_parity_difference
+DATA_SET = load_german_dataset()
+PRIVILEGED_GROUPS = [{'age': 1}]
+UNPRIVILEGED_GROUPS = [{'age': 0}]
+
 
 def run_experiment():
     print("Running SVM")
-    result = svm_experiment()
+    result = svm_experiment(num_generations=NUM_GENERATIONS, population_size=POPULATION_SIZE,
+                            mutation_rate=MUTATION_RATE, crossover_rate=CROSSOVER_RATE,
+                            chromosome_length=CHROMOSOME_LENGTH, fairness_metric=FAIRNESS_METRIC,
+                            data_set=DATA_SET, privileged_groups=PRIVILEGED_GROUPS,
+                            unprivileged_groups=UNPRIVILEGED_GROUPS)
     print('Results: ' + str(result))
     print("Running SVM with Reweighing")
-    result = svm_reweighing_experiment()
+    result = svm_reweighing_experiment(num_generations=NUM_GENERATIONS, population_size=POPULATION_SIZE,
+                                       mutation_rate=MUTATION_RATE, crossover_rate=CROSSOVER_RATE,
+                                       chromosome_length=CHROMOSOME_LENGTH, fairness_metric=FAIRNESS_METRIC,
+                                       data_set=DATA_SET, privileged_groups=PRIVILEGED_GROUPS,
+                                       unprivileged_groups=UNPRIVILEGED_GROUPS)
     print('Results: ' + str(result))
     print("Running SVM with DisparateImpactRemover")
-    result = svm_dir_experiment()
+    result = svm_dir_experiment(num_generations=NUM_GENERATIONS, population_size=POPULATION_SIZE,
+                                mutation_rate=MUTATION_RATE, crossover_rate=CROSSOVER_RATE,
+                                chromosome_length=CHROMOSOME_LENGTH, fairness_metric=FAIRNESS_METRIC,
+                                data_set=DATA_SET, privileged_groups=PRIVILEGED_GROUPS,
+                                unprivileged_groups=UNPRIVILEGED_GROUPS)
     print('Results: ' + str(result))
     print("Running SVM with Calibrated Eq Odds")
-    result = svm_caleqodds_experiment()
+    result = svm_caleqodds_experiment(num_generations=NUM_GENERATIONS, population_size=POPULATION_SIZE,
+                                      mutation_rate=MUTATION_RATE, crossover_rate=CROSSOVER_RATE,
+                                      chromosome_length=CHROMOSOME_LENGTH, fairness_metric=FAIRNESS_METRIC,
+                                      data_set=DATA_SET, privileged_groups=PRIVILEGED_GROUPS,
+                                      unprivileged_groups=UNPRIVILEGED_GROUPS)
     print('Results: ' + str(result))
     print("Running SVM with ROC")
-    result = svm_roc_experiment()
+    result = svm_roc_experiment(num_generations=NUM_GENERATIONS, population_size=POPULATION_SIZE,
+                                mutation_rate=MUTATION_RATE, crossover_rate=CROSSOVER_RATE,
+                                chromosome_length=CHROMOSOME_LENGTH, fairness_metric=FAIRNESS_METRIC,
+                                data_set=DATA_SET, privileged_groups=PRIVILEGED_GROUPS,
+                                unprivileged_groups=UNPRIVILEGED_GROUPS)
     print('Results: ' + str(result))
 
 

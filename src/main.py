2cd1dd81782ad1d5c172f2707615a997496ddec6
Pernille Johnsen <pernillej11@gmail.com>
2020-04-20 11:18:08 +0200

    Added feature selection for optimPreproc

--- src/main.py
+++ src/main.py
@@ -29,12 +29,12 @@ Customize...
 Configuration
 """
 
-NUM_GENERATIONS = 10
-POPULATION_SIZE = 10
+NUM_GENERATIONS = 100
+POPULATION_SIZE = 50
 MUTATION_RATE = 0.05
 CROSSOVER_RATE = 0.7
-CHROMOSOME_LENGTH = 30 + 57  # 15 each for C and gamma, plus 57 for the number of features in german data set
-OPTIM_PREPROC_CHROMOSOME_LENGTH = 30  # 15 each for C and gamma, no feature selection
+CHROMOSOME_LENGTH = 30 + 57  # 15 each for C and gamma, + 57 for the number of features in german data set
+OPTIM_PREPROC_CHROMOSOME_LENGTH = 30 + 11  # 15 each for C and gamma, + 11 for the num features in preproc german data
 DATA_SET = load_german_dataset()
 OPTIM_PREPROC_DATA_SET = load_preproc_data_german()
 PRIVILEGED_GROUPS = [{'age': 1}]
@@ -87,3 +87,4 @@ def plot():
 
 run_experiment()
 # plot()
+

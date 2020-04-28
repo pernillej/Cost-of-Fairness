f020c5fe392f0110aad808abd770d10e90b7a0f6
Pernille Johnsen <pernillej11@gmail.com>
2020-04-28 10:12:06 +0200

    More results, this time with Theil Index.

--- src/experiment1/main.py
+++ src/experiment1/main.py
@@ -23,7 +23,7 @@ SVM_MAX_ITER = 10000
 SVM_SEED = 0
 PRIVILEGED_GROUPS = [{'race': 1}]
 UNPRIVILEGED_GROUPS = [{'race': 0}]
-FAIRNESS_METRIC = statistical_parity_difference
+FAIRNESS_METRIC = theil_index
 ACCURACY_METRIC = binary_accuracy
 
 
@@ -65,31 +65,19 @@ def run_experiments():
 
 
 def plot():
-    svm_results = read_result_from_file('svm_24-04-2020_10-08.txt')
-    svm_results1 = read_result_from_file('svm_24-04-2020_20-59.txt')
-    svm_results2 = read_result_from_file('svm_26-04-2020_01-28.txt')
-    svm_results3 = read_result_from_file('svm_26-04-2020_15-15.txt')
-    svm_results4 = read_result_from_file('svm_27-04-2020_00-43.txt')
-    reweighing_results = read_result_from_file('svm_reweighing_24-04-2020_12-30.txt')
-    reweighing_results1 = read_result_from_file('svm_reweighing_24-04-2020_23-07.txt')
-    reweighing_results2 = read_result_from_file('svm_reweighing_26-04-2020_03-50.txt')
-    reweighing_results3 = read_result_from_file('svm_reweighing_26-04-2020_17-16.txt')
-    reweighing_results4 = read_result_from_file('svm_reweighing_27-04-2020_02-47.txt')
-    dir_results = read_result_from_file('svm_dir_25-04-2020_16-41.txt')
-    dir_results1 = read_result_from_file('svm_dir_25-04-2020_23-06.txt')
-    dir_results2 = read_result_from_file('svm_dir_26-04-2020_06-15.txt')
-    dir_results3 = read_result_from_file('svm_dir_26-04-2020_19-27.txt')
-    dir_results4 = read_result_from_file('svm_dir_27-04-2020_05-01.txt')
-    optimpreproc_results = read_result_from_file('svm_optimpreproc_24-04-2020_18-33.txt')
-    optimpreproc_results1 = read_result_from_file('svm_optimpreproc_25-04-2020_02-16.txt')
-    optimpreproc_results2 = read_result_from_file('svm_optimpreproc_26-04-2020_07-38.txt')
-    optimpreproc_results3 = read_result_from_file('svm_optimpreproc_26-04-2020_20-49.txt')
-    optimpreproc_results4 = read_result_from_file('svm_optimpreproc_27-04-2020_06-26.txt')
+    svm_results = read_result_from_file('svm_27-04-2020_16-13.txt')
+    svm_results1 = read_result_from_file('svm_28-04-2020_00-49.txt')
+    reweighing_results = read_result_from_file('svm_reweighing_27-04-2020_18-26.txt')
+    reweighing_results1 = read_result_from_file('svm_reweighing_28-04-2020_03-09.txt')
+    dir_results = read_result_from_file('svm_dir_27-04-2020_20-49.txt')
+    dir_results1 = read_result_from_file('svm_dir_28-04-2020_05-30.txt')
+    optimpreproc_results = read_result_from_file('svm_optimpreproc_27-04-2020_22-29.txt')
+    optimpreproc_results1 = read_result_from_file('svm_optimpreproc_28-04-2020_07-27.txt')
 
-    # plot_results([svm_results, svm_results1, svm_results2, svm_results3, svm_results4])
-    # plot_results([reweighing_results, reweighing_results1, reweighing_results2, reweighing_results3, reweighing_results4])
-    # plot_results([dir_results, dir_results1, dir_results2, dir_results3, dir_results4])
-    # plot_results([optimpreproc_results, optimpreproc_results1, optimpreproc_results2, optimpreproc_results3, optimpreproc_results4])
+    # plot_results([svm_results, svm_results1])
+    # plot_results([reweighing_results, reweighing_results1])
+    # plot_results([dir_results, dir_results1])
+    # plot_results([optimpreproc_results, optimpreproc_results1])
 
 
 run_experiments()
